9192e3bde2385baeb01df7e0cae70bd104cbaad6
tam59177 <tammshe186670@fpt.edu.vn>
2025-03-11 15:29:04 +0700

    Create get list leave request in leave request DBContext

--- TAMHE186670-Assignment/src/java/controller/leaverequest/LeaveRequestController.java
+++ TAMHE186670-Assignment/src/java/controller/leaverequest/LeaveRequestController.java
@@ -5,10 +5,13 @@
 package controller.leaverequest;
 
 import controller.authentication.BaseRequiredAuthenticationController;
+import dal.LeaveRequestDBContext;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import model.LeaveRequest;
 import model.User;
 
 public class LeaveRequestController extends BaseRequiredAuthenticationController {
@@ -20,7 +23,22 @@ public class LeaveRequestController extends BaseRequiredAuthenticationController
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
-        resp.sendRedirect("view/request/request.jsp");
+        int pagesize = 10;
+        String raw_pageindex = req.getParameter("page");
+        if(raw_pageindex ==null || raw_pageindex.length()==0)
+            raw_pageindex = "1";
+        int pageindex = Integer.parseInt(raw_pageindex);
+        
+        LeaveRequestDBContext db = new LeaveRequestDBContext();
+        ArrayList<LeaveRequest> lrList = db.list(pageindex,pagesize);
+        db = new LeaveRequestDBContext();
+        int count = db.count();
+        int totalpage = (count % pagesize ==0)?(count/pagesize):(count/pagesize)+1;
+        
+        request.setAttribute("totalpage", totalpage);
+        request.setAttribute("pageindex", pageindex);
+        request.setAttribute("dummies", dummies);
+        request.getRequestDispatcher("../view/dummy/list.jsp").forward(request, response);
     }
 
 }
--- TAMHE186670-Assignment/src/java/dal/LeaveRequestDBContext.java
+++ TAMHE186670-Assignment/src/java/dal/LeaveRequestDBContext.java
@@ -15,6 +15,72 @@ import model.User;
 
 public class LeaveRequestDBContext extends DBContext<LeaveRequest> {
 
+    public ArrayList<LeaveRequest> list(int pageindex, int pagesize) {
+        ArrayList lrList = new ArrayList();
+        try {
+            String sql = "SELECT lr.[lrid]\n"
+                    + "      ,lr.[title]\n"
+                    + "      ,lr.[reason]\n"
+                    + "      ,lr.[from]\n"
+                    + "      ,lr.[to]\n"
+                    + "      ,lr.[status]\n"
+                    + "      ,u.[username] as [createdbyusername]\n"
+                    + "	  ,u.[displayname] as [createdbydisplayname]\n"
+                    + "      ,lr.[createddate]\n"
+                    + "      ,e.eid\n"
+                    + "	  ,e.ename\n"
+                    + "      ,p.[username] as [processedbyusername]\n"
+                    + "	  ,p.[displayname] as [processedbydisplayname]\n"
+                    + "  FROM [LeaveRequests] lr\n"
+                    + "	INNER JOIN Users u ON u.username = lr.createby\n"
+                    + "	INNER JOIN Employees e ON e.eid = lr.owner_eid\n"
+                    + "	INNER JOIN Departments d ON d.did = e.did\n"
+                    + "	LEFT JOIN Users p ON p.username = lr.processedby\n"
+                    + " ORDER BY lrid DESC \n"
+                    + "OFFSET (?-1)*? ROWS\n"
+                    + "FETCH NEXT ? ROWS ONLY;";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, pageindex);
+            stm.setInt(2, pagesize);
+            stm.setInt(3, pagesize);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                LeaveRequest lr = new LeaveRequest();
+                lr.setId(rs.getInt("lrid"));
+                lr.setTitle(rs.getString("title"));
+                lr.setReason(rs.getString("reason"));
+                lr.setFrom(rs.getDate("from"));
+                lr.setTo(rs.getDate("to"));
+                lr.setStatus(rs.getString("status"));
+                lr.setCreateddate(rs.getTimestamp("createddate"));
+
+                User createdby = new User();
+                createdby.setUsername(rs.getString("createdbyusername"));
+                createdby.setDisplayname(rs.getString("createdbydisplayname"));
+                lr.setCreatedby(createdby);
+
+                String processbyusername = rs.getString("processedbyusername");
+                if (processbyusername != null) {
+                    User processby = new User();
+                    processby.setUsername(processbyusername);
+                    processby.setDisplayname(rs.getString("processedbydisplayname"));
+                    lr.setProcessedby(processby);
+                }
+                lrList.add(lr);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(LeaveRequestDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (connection != null)
+                try {
+                connection.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(LeaveRequestDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return lrList;
+    }
+
     @Override
     public ArrayList<LeaveRequest> list() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
@@ -188,26 +254,28 @@ public class LeaveRequestDBContext extends DBContext<LeaveRequest> {
 
     public static void main(String[] args) {
         LeaveRequestDBContext dao = new LeaveRequestDBContext();
-        LeaveRequest model = new LeaveRequest();
-        model.setTitle("Tam xin nghi hoc");
-        model.setReason("Tai vi em bi om");
-        model.setStatus("Inprogress");
+//        LeaveRequest model = new LeaveRequest();
+//        model.setTitle("Tam xin nghi hoc");
+//        model.setReason("Tai vi em bi om");
+//        model.setStatus("Inprogress");
+//
+//        Employee e = new Employee();
+//        e.setId(1);
+//        model.setOwner(e);
+//
+//        User createdBy = new User();
+//        createdBy.setUsername("tam");
+//        model.setCreatedby(createdBy);
+//
+//        LocalDate from = LocalDate.of(2025, 3, 4);
+//        LocalDate to = LocalDate.of(2025, 3, 6);
+//
+//        model.setFrom(Date.valueOf(from));
+//        model.setTo(Date.valueOf(to));
+//
+//        dao.insert(model);
 
-        Employee e = new Employee();
-        e.setId(1);
-        model.setOwner(e);
-
-        User createdBy = new User();
-        createdBy.setUsername("tam");
-        model.setCreatedby(createdBy);
-
-        LocalDate from = LocalDate.of(2025, 3, 4);
-        LocalDate to = LocalDate.of(2025, 3, 6);
-
-        model.setFrom(Date.valueOf(from));
-        model.setTo(Date.valueOf(to));
-
-        dao.insert(model);
+        System.out.println(dao.list(1, 1));
     }
 
 }
